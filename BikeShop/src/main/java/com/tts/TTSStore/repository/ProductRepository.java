cfc6bfd28ef8d973714e9273d3af3a49e5d44b1c
kyle-hoover <kylehoover76@gmail.com>
2020-12-13 17:24:27 -0500

    Trying to fix issues with undefined bean

--- BikeShop/src/main/java/com/tts/TTSStore/repository/ProductRepository.java
+++ BikeShop/src/main/java/com/tts/TTSStore/repository/ProductRepository.java
@@ -2,7 +2,7 @@ package com.tts.TTSStore.repository;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
