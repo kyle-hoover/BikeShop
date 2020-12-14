48777208382d8173cfebf14dac77ae3e61cb23e6
kyle-hoover <kylehoover76@gmail.com>
2020-12-13 20:25:39 -0500

    Cleaned up Models
    Deleted getters/setters (imported lombok properly).

--- BikeShop/src/main/java/com/tts/TTSStore/model/Product.java
+++ BikeShop/src/main/java/com/tts/TTSStore/model/Product.java
@@ -1,98 +1,34 @@
 package com.tts.TTSStore.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
 	//id that auto increments
 	@Id
-	private Long id;
-	
-	//int quantity
-	private Integer quantity;
-	
-	//price
-	private Integer price;
-	
-	//description
-	private String description;
-	
-	//name
-	private String name;
-	
-	//brand
-	private String brand;
-	
-	//category
-	private String category;
-	
-	//image(url)
-	private String image;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-	
-	//creating getters and setters to hopefully solve issues in controllers
-	
+    private int quantity;
+    private int price;
+    
+    private String name;
+    private String description;
+    private String brand;
+    private String category;
+    private String image;
 }
--- BikeShop/src/main/java/com/tts/TTSStore/model/User.java
+++ BikeShop/src/main/java/com/tts/TTSStore/model/User.java
@@ -1,16 +1,32 @@
 package com.tts.TTSStore.model;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Map;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Transient;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,14 +44,40 @@ public class User implements UserDetails{
 
 	//Auto-generated id
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
 	private Long id;
+	
+	@Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
+    private String email;
 	
 	//Strings for username and password (they can't be null)
 	@NotEmpty(message = "Please provide your username")
+	@Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
 	private String username;
 	
 	@NotEmpty(message = "Please enter your password")
+	@Length(min = 5, message = "Your password must have at least 5 characters")
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
+	
+	@NotEmpty(message = "Please provide your first name")
+    private String firstName;
+
+    @NotEmpty(message = "Please provide your last name")
+    private String lastName;
+    
+    private int active;
+    
+    @CreationTimestamp
+    private Date createdAt;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 	
 	//A Map that uses Product and Integer pairs called "cart" that is implemented by a HashMap.
 	//Use the `@ElementCollection` tag: It basically makes the map into a table with a Foreign Key to a Product and the quantity as a number without making a new model. (very similar to @OneToMany)
@@ -58,31 +100,6 @@ public class User implements UserDetails{
 	  
 	@Transient
 	private Collection<GrantedAuthority> authorities = null;
-
-	//creating getters and settters to try to resolve errors in other classes
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Map<Product, Integer> getCart() {
-		return cart;
-	}
-
-	public void setCart(Map<Product, Integer> cart) {
-		this.cart = cart;
-	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
