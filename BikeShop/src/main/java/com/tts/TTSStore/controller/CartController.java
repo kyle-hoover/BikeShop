e6d5f51c8bc1b03b761dac337df7f9a827c1360c
kyle-hoover <kylehoover76@gmail.com>
2020-12-13 20:34:46 -0500

    Updated CartController per In Class Examples
    Changed Cart update settings and removed unused imports.

--- BikeShop/src/main/java/com/tts/TTSStore/controller/CartController.java
+++ BikeShop/src/main/java/com/tts/TTSStore/controller/CartController.java
@@ -1,16 +1,15 @@
 package com.tts.TTSStore.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,9 +35,11 @@ public class CartController {
   @ModelAttribute("cart")
   public Map<Product, Integer> cart() {
       User user = loggedInUser();
-      if(user == null) return null;
-      System.out.println("Getting cart");
-      return user.getCart();
+      if(user == null) 
+    	  return null;
+      
+      Map<Product, Integer> userCart = new HashMap<Product, Integer>(user.getCart());
+      return userCart;
   }
   
   /**
@@ -56,33 +57,36 @@ public class CartController {
 
   @PostMapping("/cart")
   public String addToCart(@RequestParam long id) {
-      Product p = productService.findById(id);
-      setQuantity(p, cart().getOrDefault(p, 0) + 1);
-      return "cart";
+	  System.out.println(id);
+	  Product p = productService.findById(id);
+	  Map<Product, Integer> cart = cart();
+	  System.out.println("Posting to cart" + cart);
+	  setQuantity(p, cart.getOrDefault(p, 0) + 1);
+	  return "redirect:/cart";
   }
 
-  @PatchMapping("/cart")
+  @PostMapping("/cart/update")
   public String updateQuantities(@RequestParam long[] id, @RequestParam int[] quantity) {
-      for(int i = 0; i < id.length; i++) {
-          Product p = productService.findById(id[i]);
-          setQuantity(p, quantity[i]);
-      }
-      return "cart";
-  }
+	  for (int i = 0; i < id.length; i++) {
+			Product p = productService.findById(id[i]);
+			setQuantity(p, quantity[i]);
+		}
+		return "redirect:/cart";
+	}
 
-  @DeleteMapping("/cart")
+  @PostMapping("/cart/delete")
   public String removeFromCart(@RequestParam long id) {
-        Product p = productService.findById(id);
-        setQuantity(p, 0);
-        return "cart";
-    }
+	  Product p = productService.findById(id);
+	  setQuantity(p, 0);
+	  return "redirect:/cart";
+	}
 
   private void setQuantity(Product p, int quantity) {
-        if(quantity > 0)
-            cart().put(p, quantity);
-        else
-            cart().remove(p);
+      if(quantity > 0)
+    	  cart().put(p, quantity);
+      else
+          cart().remove(p);
 
-        userService.updateCart(cart());
+      userService.updateCart(cart());
     }
 }
